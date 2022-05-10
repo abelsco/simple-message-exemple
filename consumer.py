0df3445d7efe6818b928679696eb06bb3cdef913
Abel Menezes <abelsmenezes@gmail.com>
2022-05-10 01:32:17 -0300

    refatorando o código

--- consumer.py
+++ consumer.py
@@ -7,7 +7,7 @@ import time
 
 
 def DefinicaoEnv():
-load_dotenv()
+    load_dotenv()
     config = {
         "user": os.getenv("RABBITMQ_USER"),
         "passwd": os.getenv("RABBITMQ_PASS"),
@@ -83,24 +83,42 @@ class PubSub:
         self.canal = self.conexao.channel()
         self.RecebeMensagem()
 
-crdentials = pika.PlainCredentials(user, passwd)
+    def AbrirConexao(self):
+        credenciais = pika.PlainCredentials(
+            self.config["user"], self.config["passwd"])
+        conexao = pika.BlockingConnection(pika.ConnectionParameters(
+            self.config["server"], self.config["port"], '/', credenciais))
+        return conexao
+
+    def RecebeMensagem(self):
+        self.canal.exchange_declare(
+            exchange=self.config["exchange"], exchange_type='fanout')
+
+        queue_local = self.canal.queue_declare(
+            queue='', exclusive=True).method.queue
+
+        self.canal.queue_bind(
+            exchange=self.config["exchange"], queue=queue_local)
+
+        def callback(ch, method, properties, body):
+            print(f" [x] Opa recebi a mensagem {body}")
+
+        self.canal.basic_consume(
+            queue=queue_local, on_message_callback=callback, auto_ack=True)
+
+        print(' [*] Esperando por mensagens. CTRL+C para sair')
+        self.canal.start_consuming()
 
 
 def main():
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(server, port, '/', crdentials))
-    channel = connection.channel()
-
-    channel.queue_declare(queue=queue)
-
-    def callback(ch, method, properties, body):
-        print(f" [x] Opa recebi a mensagem {body}")
-
-    channel.basic_consume(
-        queue=queue, on_message_callback=callback, auto_ack=True)
-
-    print(' [*] Esperando por mensagens. CTRL+C para sair')
-    channel.start_consuming()
+    mode = sys.argv[1].upper()
+    print(f'MODE: {mode}')
+    if mode == 'PUB_SUB':
+        PubSub()
+    elif mode == 'WORK_QUEUES':
+        WorkQueues()
+    else:
+        MensageriaSimples()
 
 
 if __name__ == '__main__':
